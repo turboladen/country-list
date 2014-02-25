15186b222fffaaf0ac27f58733215d6499928d9a
Steve Loveless <steve.loveless@gmail.com>
2014-02-24 16:39:26 -0800

    Modernized the generator for the migration

--- lib/generators/countries_list/countries_list_generator.rb
+++ lib/generators/countries_list/countries_list_generator.rb
@@ -1,36 +1,15 @@
-require 'rails/generators/active_record'
+require 'rails/generators/base'
 
-class CountriesListGenerator < ActiveRecord::Generators::Base
-=begin
-  def create_migration_file
-    migrations_directory_path = "#{RAILS_ROOT}/db/migrate"
-    Dir.mkdir(migrations_directory_path) unless File.directory?(migrations_directory_path)
+class CountriesListGenerator < Rails::Generators::Base
+  include Rails::Generators::Migration
 
-    # copy countries migration
-    record do |m|
-      m.file 'migrations/migration.rb', "db/migrate/#{Time.now.strftime('%Y%m%d%H%M%S')}_create_countries.rb"
-    end
+  source_root File.expand_path('../templates', __FILE__)
+
+  def self.next_migration_number(_)
+    Time.now.strftime('%Y%m%d%H%M%S')
   end
-=end
 
   def copy_countries_migration
-    if (behavior == :invoke && model_exists?) || (behavior == :revoke && migration_exists?(table_name))
-      migration_template 'migration.rb', "db/migrate/create_#{table_name}"
-    end
-  end
-
-
-  def generate_model
-    invoke 'active_record:model', [name], :migration => false unless model_exists? && behavior == :invoke
-  end
-
-  private
-
-  def model_exists?
-    File.exists?(File.join(destination_root, model_path))
-  end
-
-  def model_path
-    @model_path ||= File.join('app', 'models', "#{file_path}.rb")
+    migration_template 'migration.rb', 'db/migrate/create_countries'
   end
 end
