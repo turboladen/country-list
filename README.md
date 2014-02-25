4289fc3509b20694d2df5bfa9cf157d3eb340211
Steve Loveless <steve.loveless@gmail.com>
2014-02-24 17:29:51 -0800

    Nicer generator

--- README.md
+++ README.md
@@ -4,11 +4,15 @@ Country List
 Initially forked from mm1/country-list, but updated to work a little nicer with
 Rails 4, bundler, and such.
 
-It provides a generator that will add a new migration file to your migrations folder
-This file code will create "countries" table and fill it with countries
+It provides a generator that will create:
 
-This creates a migration that contains a list of countries based on ISO 3166.
-Each country row contains:
+* a migration to create the countries table
+* the Country model
+* db/seed_countries.rb, which has the code for seeding the country data
+* lib/tasks/seed_countries.rake, which runs the code in db/seed_countries.rb
+
+The migration contains a list of countries based on ISO 3166. Each country row
+contains:
 
 * short name
 * alpha-2 code
--- lib/generators/countries_list/countries_list_generator.rb
+++ lib/generators/countries_list/countries_list_generator.rb
@@ -10,6 +10,35 @@ class CountriesListGenerator < Rails::Generators::Base
   end
 
   def copy_countries_migration
-    migration_template 'migration.rb', 'db/migrate/create_countries'
+    migration_template 'migration.rb', 'db/migrate/create_countries_list'
+  end
+
+  def generate_model
+    invoke 'active_record:model', ['country'], migration: false unless model_exists? && behavior == :invoke
+  end
+
+  def copy_countries_seeds
+    copy_file 'seeds.rb', 'db/seed_countries.rb'
+  end
+
+  def create_rake_task
+    rakefile 'seed_countries.rake' do
+      %q{
+desc 'Create the countries table and seed it with data'
+task seed_countries: :environment do
+  load 'db/seed_countries.rb'
+end
+      }
+    end
+  end
+
+  private
+
+  def model_exists?
+    File.exists?(File.join(destination_root, model_path))
+  end
+
+  def model_path
+    @model_path ||= File.join("app", "models", "country.rb")
   end
 end
